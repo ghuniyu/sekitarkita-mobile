fba3c7a0b1cbf7dd7dfe88d1ed6ef7d9d67cebe1
muhammad ilham <muhammad.ilham@ritase.com>
2020-03-29 17:21:26 +0700

    done: minify, shrink res, turning log off

--- app/src/main/java/id/ghuniyu/sekitar/data/remote/Client.kt
+++ app/src/main/java/id/ghuniyu/sekitar/data/remote/Client.kt
@@ -14,8 +14,13 @@ object Client {
 
         val gsonConverter = GsonConverterFactory.create(gson)
 
-        val loggingInterceptor: HttpLoggingInterceptor = HttpLoggingInterceptor()
-            .setLevel(HttpLoggingInterceptor.Level.BODY)
+        val loggingInterceptor: HttpLoggingInterceptor = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BODY)
+        } else {
+            HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.NONE)
+        }
 
         val client = OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
