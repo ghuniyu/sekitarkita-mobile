595c1378465ca54379ea21db891ea56b0e59c3a2
iamnubs <ghuniyu.fattah.rozaq@gmail.com>
2020-05-22 13:00:55 +0700

    Add search bar

--- app/src/main/java/com/linkensky/ornet/presentation/statistic/pages/HospitalController.kt
+++ app/src/main/java/com/linkensky/ornet/presentation/statistic/pages/HospitalController.kt
@@ -2,9 +2,16 @@ package com.linkensky.ornet.presentation.statistic.pages
 
 import com.linkensky.ornet.hospital
 import com.linkensky.ornet.presentation.base.BaseController
+import com.linkensky.ornet.searchBar
 
 class HospitalController : BaseController(){
     override fun buildModels() {
+
+        searchBar {
+            id("search-bar")
+            hint("Rumah sakit / Kota / Provinsi")
+        }
+
         1.rangeTo(100).forEach {i ->
             hospital {
                 id("hospital-$i")
