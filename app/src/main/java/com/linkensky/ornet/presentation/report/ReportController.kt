8c24f6638965fdfd632219af73058154625a68b8
iamnubs <ghuniyu.fattah.rozaq@gmail.com>
2020-05-23 13:39:14 +0700

    update report view

--- app/src/main/java/com/linkensky/ornet/presentation/report/ReportController.kt
+++ app/src/main/java/com/linkensky/ornet/presentation/report/ReportController.kt
@@ -1,10 +1,7 @@
 package com.linkensky.ornet.presentation.report
 
 import androidx.annotation.ColorRes
-import com.linkensky.ornet.R
-import com.linkensky.ornet.presentation.base.BaseController
 import com.linkensky.ornet.presentation.base.MvRxEpoxyController
-import com.linkensky.ornet.report
 
 data class Report(
     @ColorRes
@@ -14,20 +11,5 @@ data class Report(
 )
 
 class ReportController : MvRxEpoxyController() {
-    override fun buildModels() {
-        val reports = arrayOf(
-            Report(R.color.colorDanger, "odp", "Saya adalah ODP"),
-            Report(R.color.colorAccent, "pdp", "Saya adalah PDP"),
-            Report(R.color.colorPrimary, "positive", "Saya adalah POSITIF"),
-            Report(R.color.blue, "healthy", "Saya adalah SEHAT")
-        )
-
-        reports.map {
-            report {
-                id(it.value)
-                cardBg(it.bg)
-                title(it.title)
-            }
-        }
-    }
+    override fun buildModels() {}
 }
--- app/src/main/java/com/linkensky/ornet/presentation/report/ReportFragment.kt
+++ app/src/main/java/com/linkensky/ornet/presentation/report/ReportFragment.kt
@@ -2,30 +2,26 @@ package com.linkensky.ornet.presentation.report
 
 import android.os.Bundle
 import android.view.View
-import androidx.recyclerview.widget.GridLayoutManager
 import com.linkensky.ornet.R
 import com.linkensky.ornet.databinding.FragmentReportBinding
-import com.linkensky.ornet.presentation.base.BaseEpoxyBindingFragment
 import com.linkensky.ornet.presentation.base.BaseFragment
-import com.linkensky.ornet.presentation.base.MvRxEpoxyController
 
-class ReportFragment : BaseEpoxyBindingFragment() {
+class ReportFragment : BaseFragment<FragmentReportBinding>() {
     private val controller by lazy {
         ReportController()
     }
 
-    override val toolbarTitle: String = "Lapor Infeksi"
+    override fun getLayoutRes() = R.layout.fragment_report
+    override fun invalidate() {}
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
-            recyclerView.layoutManager = GridLayoutManager(context, 2)
-            setOnInfoClick { _ ->
-                navigateTo(R.id.action_reportFragment_to_macAddressFragment)
-            }
+            lifecycleOwner = viewLifecycleOwner
+
+            text = "Lapor Infeksi"
+            controller.requestModelBuild()
         }
     }
-
-    override fun epoxyController() = controller
 }
